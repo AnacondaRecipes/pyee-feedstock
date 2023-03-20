1c91d7e1d7428562279a979e734dd5ce2678ebbf
Kai Tietz <ktietz@anaconda.com>
2023-03-20 15:14:58 +0100

    move import test into run_test.py

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,6 +1,7 @@
+import pyee
+
 import os
 import pkg_resources
-
 
 assert pkg_resources.get_distribution("pyee").version == os.environ["PKG_VERSION"], \
     "package version does not match expected version"
