4fd24b60bc48cf92183b9f40999a0f231bc4d0e3
Patryk Kups <pkups@anaconda.com>
2025-12-02 12:17:21 +0100

    check

--- recipe/run_test.py
+++
@@ -1,7 +0,0 @@
-import pyee
-
-import os
-import pkg_resources
-
-assert pkg_resources.get_distribution("pyee").version == os.environ["PKG_VERSION"], \
-    "package version does not match expected version"
